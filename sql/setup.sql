bae9a79526741572aa10eb09e158399cc54086d0
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-09 15:27:45 -0700

    added planets info to setup file

--- sql/setup.sql
+++ sql/setup.sql
@@ -8,17 +8,15 @@ CREATE table cats (
   name VARCHAR NOT NULL,
   type VARCHAR NOT NULL,
   url VARCHAR,
-  year INT NOT NULL,
-  lives INT NOT NULL,
-  isSidekick BOOLEAN
-);
+  distance_from_sun INT NOT NULL,
+  orbital_period_in_days INT NOT NULL);
 
-INSERT INTO cats (name, type, url, year, lives, isSidekick) VALUES 
-('Felix','Tuxedo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png', 1892, 3, false),
-('Garfield','Orange Tabby', 'https://static.wikia.nocookie.net/garfield/images/9/9f/GarfieldCharacter.jpg', 1978, 7, false),
-('Duchess','Angora', 'https://static.wikia.nocookie.net/disney/images/e/eb/Profile_-_Duchess.jpeg', 1970, 9, false),
-('Stimpy','Manx','https://static.wikia.nocookie.net/renandstimpy/images/c/c1/Ren-stimpy-25-anniversar-hp2.png', 1990, 1, true),
-('Sylvester','Tuxedo', 'https://static.wikia.nocookie.net/charactercommunity/images/7/73/SylvesterDance.png',1945, 1, true),
-('Tigger','Tiger', 'https://www.pinclipart.com/picdir/big/150-1504133_tigger-tigger-cartoon-me-clipart-png-image-download.png', 1928, 8, false),
-('Hello Kitty','Angora', 'https://cdn.shopify.com/s/files/1/0054/4371/5170/products/FiGPiN_360HelloKittySANRIOPIN.png?v=1627413934', 1974, 9, false),
-('Hobbes','Tiger', 'https://sketchok.com/images/articles/01-cartoons/000-va/102/10.jpg', 1985, 6, true);
+INSERT INTO cats (name, type, url, distance_from_sun, orbital_period_in_days) VALUES 
+('Mercury','terrestrial', 'https://cdn.mos.cms.futurecdn.net/PFQ97KNjjTebMzenT3GeKd-1200-80.jpg', 41.227, 88),
+('Venus','terrestrial', 'https://cdn.mos.cms.futurecdn.net/RifjtkFLBEFgzkZqWEh69P.jpg', 67.24, 225),
+('Earth','terrestrial', 'https://www.universetoday.com/wp-content/uploads/2009/09/bluemarble.jpg', 92.96, 365),
+('Mars','terrestrial','https://cdn.mos.cms.futurecdn.net/L868HCTDT8ueAXXrcyXoWc.jpg', 141.6, 687),
+('Jupiter','gas giant', 'https://cdn.mos.cms.futurecdn.net/yyonzpUYYkQNX8corCxeyD.jpeg', 483.8, 4380),
+('Saturn','gas giant', 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg', 890.8, 9490),
+('Uranus','ice giant', 'https://upload.wikimedia.org/wikipedia/commons/4/48/Uranus_as_seen_by_NASA%27s_Voyager_2_%28remastered%29.png', 1784, 30660),
+('Neptune','ice giant', 'https://cdn.mos.cms.futurecdn.net/NrpYyu3sEmB5pcry9rWMk8.jpg', 2793, 60225)
